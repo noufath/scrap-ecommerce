c7605cfc0e3dffc90abf19e5403d06cc7596d4cd
Dany <danychristianto@gmail.com>
2020-09-15 16:57:52 +0700

    1. login.py 2. get_url.py 3. get_detail.py

--- run.py
+++ run.py
@@ -1,12 +1,93 @@
-from scrap.getdata import GetData
+import requests
+import json
+from bs4 import BeautifulSoup
+
+session = requests.Session()
 
 
 def login():
-    print("Login..")
+    dt_login = {
+        'username': 'user',
+        'password': 'user12345'
+    }
+    post_response = session.post("http://localhost:5000/login", data=dt_login)
+    soup = BeautifulSoup(post_response.text, "html5lib")
+    page_item = soup.find_all('li', attrs={'class': 'page-item'})
+    tot_page = len(page_item) - 2
+    return tot_page
 
 
-def run():
-    login()
+def get_url(page, catch_urls=[]):
+    print('process... url page {}'.format(page))
+    par_data = {
+        'page': page
+    }
+    get_response = session.get("http://localhost:5000", params=par_data)
+
+    # f_login = open('./resp.html', 'w+')
+    # f_login.write(get_response.text)
+    # f_login.close()
+
+    soup = BeautifulSoup(get_response.text, "html5lib")
+
+    # After download html file from request, it's can use for scrap in development so, we don't need to do request every
+    # time we run program
+    # soup = BeautifulSoup(open('./resp.html'), "html5lib")
+
+    titles = soup.find_all('h4', attrs={'class': 'card-title'})
+
+    for title in titles:
+        catch_url = title.find('a')['href']
+        catch_urls.append(catch_url)
+
+    return catch_urls
+
+
+def get_detail(url):
+    print('getting detail....')
+    detail_response = session.get('http://localhost:5000'+url)
+
+    # f_login = open('./resp.html', 'w+')
+    # f_login.write(detail_response.text)
+    # f_login.close()
+
+    soup = BeautifulSoup(detail_response.text, 'html5lib')
+    title = soup.find('title').text.strip()
+    price = soup.find('h4', attrs={'class': 'card-price'}).text.strip()
+    stock = soup.find('span', attrs={'class': 'card-stock'}).text.strip().replace('stock: ', '')
+    category = soup.find('span', attrs={'class': 'card-category'}).text.strip().replace('category: ', '')
+    description = soup.find('p', attrs={'class': 'card-text'}).text.strip().replace('Description: ', '')
+
+    dict_data = {
+        'title': title,
+        'price': price,
+        'stock': stock,
+        'category': category,
+        'description': description
+    }
+
+    with open('./result/{}.json'.format(url.replace('/', '')), 'w+') as outfile:
+        json.dump(dict_data, outfile)
+
+
+def run(urls=[]):
+    pages = login()
+
+    # for i in range(pages):
+    #     page = i + 1
+    #     urls = urls + get_url(page)
+    #
+    #     with open('urls.json', 'w+') as outfile:
+    #         json.dump(urls, outfile)
+    # After json file created we can command line getting url request and use json file directly
+
+    # with open('urls.json') as json_file:
+    #    all_url = json.load(json_file)
+
+    # for url in all_url:
+    #    get_detail(url)
+
+    get_detail('/mamypoko-good-night-pants-xxl-28-girls')
 
 
 if __name__ == '__main__':
--- scrap/getdata.py
+++ scrap/getdata.py
@@ -3,17 +3,7 @@ from bs4 import BeautifulSoup
 
 
 class GetData:
-    def __init__(self, url_name, url_data):
+    def __init__(self, url_name, url_data, url_param, session_opt):
         self.url = url_name
         self.url_data = url_data
-        self.get_response = requests.get(self.url)
-        self.post_response = requests.post(self.url, self.url_data)
 
-    def scrap_status(self):
-        return self.url_response.status_code
-
-    def parsed_html(self):
-        return BeautifulSoup(self.url_response.content, 'html.parser')
-
-    def parsed_body(self):
-        return self.parsed_html().find('body')
